ff7e482c34f09bd8dcc459f153108e166e9f78fd
TheLordVier <messi060591@gmail.com>
2023-08-30 15:24:09 +0300

    changes in the test_calculator.py file

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -40,32 +40,3 @@ class TestApp(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-# from fastapi.testclient import TestClient
-# from calculator import app
-#
-#
-# # Создайте экземпляр TestClient
-# client = TestClient(app)
-#
-#
-# def test_home():
-#     # Тест для GET запроса на главную страницу
-#     response = client.get("/")
-#     assert response.status_code == 200
-#     assert "Введите выражение:" in response.text
-#
-#
-# def test_calculate_valid():
-#     # Тест для POST запроса с корректным выражением
-#     response = client.post("/", data={"expression": "2 + 2"})
-#     assert response.status_code == 200
-#     assert "Результат:" in response.text
-#
-#
-# def test_calculate_invalid():
-#     # Тест для POST запроса с некорректным выражением
-#     response = client.post("/", data={"expression": "2 / 0"})
-#     assert response.status_code == 200
-#     assert "Ошибка:" in response.text
